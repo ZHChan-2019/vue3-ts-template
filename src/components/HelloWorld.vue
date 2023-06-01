973b1ce79f1b7b8efafb3b9c2365827a0017e064
hzichan2019 <hzichan2019@gmail.com>
2023-06-01 15:27:44 +0800

    chore: 引入 element-plus 和其图标库

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,42 @@
 <script setup lang="ts">
-import { ref, reactive } from 'vue'
+import { ref } from 'vue'
 
-const count = ref(0)
-const test = reactive({
-  id: 1,
-  name: 'hello',
-})
+import { Check, Delete, Edit, Message, Star } from '@element-plus/icons-vue'
+
+const currentPage1 = ref(5)
+const small = ref(false)
+const background = ref(false)
+const disabled = ref(false)
+
+const handleSizeChange = (val: number) => {
+  console.log(`${val} items per page`)
+}
+const handleCurrentChange = (val: number) => {
+  console.log(`current page: ${val}`)
+}
 </script>
 
 <template>
-  <h1>hello</h1>
-  <div>{{ count }}</div>
-  <div>{{ test.id }}:{{ test.name }}</div>
+  <el-button>Default</el-button>
+  <el-button type="primary" :icon="Edit">Primary</el-button>
+  <el-button type="success" :icon="Check">Success</el-button>
+  <el-button type="info" :icon="Message">Info</el-button>
+  <el-button type="warning" :icon="Star">Warning</el-button>
+  <el-button type="danger" :icon="Delete">Danger</el-button>
+
+  <div>
+    <el-pagination
+      v-model:current-page="currentPage1"
+      :page-size="100"
+      :small="small"
+      :disabled="disabled"
+      :background="background"
+      layout="total, prev, pager, next"
+      :total="1000"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
+  </div>
 </template>
 
 <style scoped></style>
